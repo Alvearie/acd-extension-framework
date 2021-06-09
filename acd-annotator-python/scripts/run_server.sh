88b11433684cb84abe6d6227d4d2b28cd1a7fcbb
Paul Felt <plfelt@us.ibm.com>
2021-06-09 13:24:59 -0600

    added explicit port to server startup

--- acd-annotator-python/scripts/run_server.sh
+++ acd-annotator-python/scripts/run_server.sh
@@ -6,13 +6,13 @@
 #                                                                   #
 # ***************************************************************** #
 # Regex annotator
-uvicorn example_apps.regex_annotator:app --host 0.0.0.0 --factory --workers 5 --limit-concurrency 10 --backlog 10 \
-  --log-config ./acd_annotator_python/defaultLogSettings.json
+#uvicorn example_apps.regex_annotator:app --host 0.0.0.0 --port 8000 --factory --workers 5 --limit-concurrency 10 --backlog 10 \
+#  --log-config ./acd_annotator_python/defaultLogSettings.json
 
-## Spacy annotator requires extras to be installed
-#uvicorn example_apps.extras.spacy_sentence_annotator:app --host 0.0.0.0 --factory --workers 5 --limit-concurrency 10 --backlog 10 \
-#  --log-config ./acd_annotator_python/defaultLogSettings.json --env-file ./example_apps/extras/server.env
+# Spacy annotator requires extras to be installed
+uvicorn example_apps.extras.spacy_sentence_annotator:app --host 0.0.0.0 --port 8000 --factory --workers 5 --limit-concurrency 10 --backlog 10 \
+  --log-config ./acd_annotator_python/defaultLogSettings.json --env-file ./example_apps/extras/server.env
 
 ## Stanza annotator requires extras to be installed
-#uvicorn example_apps.extras.stanza_sentence_annotator:app --host 0.0.0.0 --factory --workers 5 --limit-concurrency 10 --backlog 10 \
+#uvicorn example_apps.extras.stanza_sentence_annotator:app --host 0.0.0.0 --port 8000 --factory --workers 5 --limit-concurrency 10 --backlog 10 \
 #  --log-config ./acd_annotator_python/defaultLogSettings.json --env-file ./example_apps/extras/server.env
